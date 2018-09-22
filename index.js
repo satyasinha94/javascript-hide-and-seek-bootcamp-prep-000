16d2ae5233e324451045dcbda339681fc6ae22b1
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-22 01:17:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 function getFirstSelector(selector) {
+<<<<<<< HEAD
   return document.querySelector(selector)
 }
 
@@ -17,4 +18,23 @@ function increaseRankBy(n) {
 function deepestChild () {
 let deepChild = document.getElementById('grand-node').querySelectorAll('div');
 return deepChild[deepChild.length - 1]
+=======
+  return document.querySelector(`${selector}`)
+}
+
+function nestedTarget () {
+  return document.querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const rankedList = document
+  .getElementById("app")
+  .querySelectorAll("ul.ranked-list li");
+
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = (i + `$(n)`).toString();
+  }
+  var parsed = parseInt(rankedList.innerHTML)
+  return parsed
+>>>>>>> c7400b75353be6a2e927158881054d77d7965c5c
 }
