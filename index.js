e82f4da31023d10115f9cad3b6954ec9274c7c53
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-22 00:21:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.getElementById('app').querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n
+  }
+  return rankedList
+}
+function deepestChild () {
+let deepChild = document.getElementById('grand-node').querySelectorAll('div');
+return deepChild[deepChild.length - 1]
+}
